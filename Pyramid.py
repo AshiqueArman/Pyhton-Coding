148f9d2020349ceaae12914cd3382cfc16026662
AshiqueArman <ashiquearman@outlook.com>
2019-04-28 12:52:35 +0600

    added comments

--- Pyramid.py
+++ Pyramid.py
@@ -1,8 +1,8 @@
-n = int(input("Rows : "))
+n = int(input("Rows : ")) #taking input
 
-for x in range(0, n):
-    for y in range(0, n - 1 - x):
+for x in range(0, n):               #number of rows. Means how tall the pyramid will be.
+    for y in range(0, n - 1 - x):   #loop for giving required spaces for the formation of pyramid
         print(end=" ")
-    for z in range(0, x + 1):
+    for z in range(0, x + 1):       #loop for printing the stars
         print("*", end=" ")
-    print()
+    print()                         #going to a new row
