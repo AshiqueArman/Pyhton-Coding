868c68d9941ead24e155f4173712374bedc361d6
AshiqueArman <ashiquearman@outlook.com>
2019-04-28 12:48:41 +0600

    Update Pyramid.py

--- Pyramid.py
+++ Pyramid.py
@@ -1,3 +1,8 @@
-for x in range(5):
-    for y in range(3):
-        print(f"({x}, {y})")
+n = int(input("Rows : "))
+
+for x in range(0, n):
+    for y in range(0, n - 1 - x):
+        print(end=" ")
+    for z in range(0, x + 1):
+        print("*", end=" ")
+    print()
